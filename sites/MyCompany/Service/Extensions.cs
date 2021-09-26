95c88fbf47548e1f440a8803b2dc2a9f018c5d0c
Your Name <you@example.com>
2021-09-27 02:15:42 +0300

    create main base for the sits

---
+++ sites/MyCompany/Service/Extensions.cs
@@ -0,0 +1,7 @@
+﻿namespace MyCompany.Service {
+    public static class Extensions {
+        public static string CutController(this string str) {
+            return str.Replace("Controller", "");
+            }
+        }
+    }
--- sites/MyCompany/Startup.cs
+++ sites/MyCompany/Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MyCompany {
    public class Startup {
@@ -17,19 +13,18 @@ namespace MyCompany {
 
       public IConfiguration Configuration { get; }
 
-      // This method gets called by the runtime. Use this method to add services to the container.
       public void ConfigureServices(IServiceCollection services) {
          services.AddRazorPages();
+         //поддержка контроллеров и представлений (MVC)
+         services.AddControllersWithViews().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
          }
 
-      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
       public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
          if (env.IsDevelopment()) {
             app.UseDeveloperExceptionPage();
             }
          else {
             app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
             }
 
@@ -40,9 +35,10 @@ namespace MyCompany {
 
          app.UseAuthorization();
 
+         //система основаная на контроллерах и представлениях
          app.UseEndpoints(endpoints => {
-            endpoints.MapRazorPages();
-         });
-         }
+            endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+          }
       }
    }
